2dc79fc65509cad7874ca13512f8f546f3b88d29
Disha <disha.wind@gmail.com>
2025-10-24 11:08:17 +0300

    Добавил парсер

---
+++ src/sentence_parser.py
@@ -0,0 +1,39 @@
+import re
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+
+class SentenceParser:
+    def __init__(self, text):
+        self.text = text
+        self._download_nltk_resources()
+
+    def _download_nltk_resources(self):
+        """Скачивает необходимые ресурсы NLTK"""
+        try:
+            nltk.data.find('tokenizers/punkt')
+        except LookupError:
+            nltk.download('punkt')
+
+        try:
+            nltk.data.find('tokenizers/punkt_tab')
+        except LookupError:
+            nltk.download('punkt_tab')
+
+    def get_tokenize_sentence(self):
+        """
+        Итератор, который возвращает токенизированные предложения без знаков препинания.
+
+        Yields:
+            list: Список токенов (слов) одного предложения
+        """
+        sentences = sent_tokenize(self.text, language='russian')
+
+        for sentence in sentences:
+            tokens = word_tokenize(sentence, language='russian')
+
+            # Удаляем только явные знаки препинания, оставляем все буквы и цифры
+            tokens = [token.lower() for token in tokens if not re.match(r'^[^\w]+$', token, re.UNICODE)]
+
+            if tokens:
+                yield tokens

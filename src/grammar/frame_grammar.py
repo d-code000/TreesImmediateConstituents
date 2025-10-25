160a8323b12b32f33a86de849261e9d17172ea8d
Disha <disha.wind@gmail.com>
2025-10-25 11:15:30 +0300

    Починил frame_grammar.py

--- src/grammar/frame_grammar.py
+++ src/grammar/frame_grammar.py
@@ -20,14 +20,25 @@ class FrameGrammar(AbstractGrammar):
         Возвращает структурные правила по фреймам
         """
         return """
-        S -> Event | Agent Event | Event Patient | Agent Event Patient | Manner Event | Agent Manner Event | Event Patient Location | Agent Event Patient Location | Manner Agent Event Patient
+                S -> Event 
+                S -> Agent Event 
+                S -> Event Patient 
+                S -> Agent Event Patient 
+                S -> Manner Event 
+                S -> Agent Manner Event 
+                S -> Event Patient Location 
+                S -> Agent Event Patient Location 
+                S -> Manner Agent Event Patient
+                S -> Agent Event Location 
+                S -> Agent Event Instrument
 
-        Event -> V | ADV V
-        Agent -> NP
-        Patient -> NP
-        Location -> PP
-        Manner -> ADV | PP
+                Event -> V | ADV V
+                Agent -> NP
+                Patient -> NP
+                Location -> PP
+                Manner -> ADV | PP
+                Instrument -> PP
 
-        NP -> N | PRON | ADJ N | ADJ ADJ N | N N
-        PP -> PREP N | PREP ADJ N | PREP NP
-        """
+                NP -> N | PRON | ADJ N | ADJ ADJ N | N N
+                PP -> PREP N | PREP ADJ N | PREP NP
+                """
